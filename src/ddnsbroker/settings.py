22fb671c00b480624460fb40b5ecf43fe815c582
Moritz Jordan <jomority@openotter.com>
2020-03-13 20:16:09 +0100

    Remove jinja2 template backend

--- src/ddnsbroker/settings.py
+++ src/ddnsbroker/settings.py
@@ -36,19 +36,6 @@ ROOT_URLCONF = 'ddnsbroker.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.jinja2.Jinja2',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': False,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-    {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
