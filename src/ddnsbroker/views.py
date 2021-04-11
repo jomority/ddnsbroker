7b27806daa94279868e1b62b96488954a81cf7ab
Moritz Jordan <jomority@openotter.com>
2021-04-11 15:28:12 +0200

    Fix client IP detection with (reverse) proxies

--- src/ddnsbroker/views.py
+++ src/ddnsbroker/views.py
@@ -14,7 +14,12 @@ logger = logging.getLogger(__name__)
 
 class RemoteIpView(View):
     def get(self, request):
-        return PlainResponse(normalize_ip(request.META.get('REMOTE_ADDR')))
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return PlainResponse(normalize_ip(ip))
 
 
 class NicUpdateView(View):
